0a43a568736454b445c8ab279e2b2752398fb112
Zahed Ali Mohammed <zahedalimohammed@Zaheds-MacBook-Air.local>
2025-12-18 13:50:04 +0400

    Configure Next.js images for better optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    unoptimized: false,
+    formats: ['image/avif', 'image/webp'],
+  },
+};
 
 export default nextConfig;
 
